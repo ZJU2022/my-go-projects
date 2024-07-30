e60f027379c13839f7b2b78b9d1e48750b3bafd3
feiwu.lin <feiwu.lin@ucloud.cn>
2024-07-30 09:56:57 +0800

    add error

--- generic-utils/internal/err/error.go
+++ generic-utils/internal/err/error.go
@@ -2,6 +2,7 @@ package err
 
 import (
 	"fmt"
+	"time"
 )
 
 // NewErrIndexOutOfRange 创建一个代表下标超出范围的错误
@@ -12,4 +13,12 @@ func NewErrIndexOutOfRange(length int, index int) error {
 // NewErrInvalidType 创建一个代表类型转换失败的错误
 func NewErrInvalidType(want string, got any) error {
 	return fmt.Errorf("类型转换失败，预期类型:%s, 实际值:%#v", want, got)
+}
+
+func NewErrInvalidIntervalValue(interval time.Duration) error {
+	return fmt.Errorf("ekit: 无效的间隔时间 %d, 预期值应大于 0", interval)
+}
+
+func NewErrInvalidMaxIntervalValue(maxInterval, initialInterval time.Duration) error {
+	return fmt.Errorf("ekit: 最大重试间隔的时间 [%d] 应大于等于初始重试的间隔时间 [%d] ", maxInterval, initialInterval)
 }
